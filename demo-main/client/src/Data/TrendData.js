27310e3db97ed890c6041a268bce5cd977884ded
Suprabhatgit <Ayxxnyrr@gmail.com>
2023-08-21 15:09:44 +0530

    CUSPACE

---
+++ demo-main/client/src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData= [
+  {
+    name: "CUSPACE",
+    shares: 97,
+  },
+  {
+    name: "Avangers",
+    shares: 80.5,
+  },
+  {
+    name: "CU PROJECT",
+    shares: 100.5,
+  },
+  {
+    name: "Reactjs",
+    shares: 72,
+  },
+  {
+    name: "Elon Musk",
+    shares: 71.9,
+  },
+  {
+    name: "Ayaan",
+    shares: 20,
+  },
+];
